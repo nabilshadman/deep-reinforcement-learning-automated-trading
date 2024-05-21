d7c568821096a0ab35fff6355814ce0377544ca6
nabilshadman <N.Shadman@sms.ed.ac.uk>
2024-05-21 14:14:46 +0100

    [dqn][ppo] test nsight systems on both gpu and cpu nodes

--- code/ppo/ppo_trader.py
+++ code/ppo/ppo_trader.py
@@ -453,7 +453,7 @@ if __name__ == '__main__':
   rewards_folder = 'rl_trader_rewards'
   N = 20
   batch_size = 32
-  num_episodes = 10
+  num_episodes = 2
   alpha = 0.0003
   initial_investment = 20000
   transaction_cost_rate=0.02
