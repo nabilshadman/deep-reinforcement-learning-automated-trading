cdd254c699f38cfbc28ad83fafb534b93c32b30c
nabilshadman <nabil.shadman@gmail.com>
2024-08-31 12:39:24 +0600

    [dqn][ppo] prepare trader programs for stock scaling experiments

--- code/dqn/scaling_stocks/dqn_trader.py
+++ code/dqn/scaling_stocks/dqn_trader.py
@@ -35,7 +35,7 @@ def set_seeds(seed=42):
 
 
 # Function to load configuration from a YAML file
-def load_config(config_file='config.yaml'):
+def load_config(config_file):
     with open(config_file, 'r') as f:
         config = yaml.safe_load(f)
     return config
@@ -418,8 +418,16 @@ if __name__ == '__main__':
   #     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
   #     print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
 
+  # Adding command-line arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-m', '--mode', type=str, required=True,
+                      help='either "train" or "test"')
+  parser.add_argument('-c', '--config', type=str, required=True,
+                        help='Path to the configuration file (YAML)')
+  args = parser.parse_args()
+
   # Load configuration
-  config = load_config('config.yaml')
+  config = load_config(args.config)
 
   # Configuration for the trading environment and simulation
   data_file = config['data_file']
@@ -438,11 +446,6 @@ if __name__ == '__main__':
   epsilon_decay = config['epsilon_decay']
   alpha = config['alpha']
   
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-m', '--mode', type=str, required=True,
-                      help='either "train" or "test"')
-  args = parser.parse_args()
-
   # determine the mode string and formatting
   mode_str = "Training Mode" if args.mode == "train" else "Testing Mode"
   # print with visual separation
@@ -563,6 +566,14 @@ if __name__ == '__main__':
   print(f"Median portfolio value (USD): {np.median(portfolio_value):.2f}")
   print(f"Minimum portfolio value (USD): {np.min(portfolio_value):.2f}")
   print(f"Maximum portfolio value (USD): {np.max(portfolio_value):.2f}")
+
+  # Calculate and print the median portfolio value of the last 30 episodes
+  if len(portfolio_value) >= 30:
+      last_30_portfolio_values = portfolio_value[-30:]
+      median_last_30 = np.median(last_30_portfolio_values)
+      print(f"Median portfolio value of last 30 episodes (USD): {median_last_30:.2f}")
+  else:
+      print("Not enough episodes to calculate median portfolio value of last 30 episodes.")
 
   # After all processing is done, calculate and print total execution time
   end_time = time.time()
